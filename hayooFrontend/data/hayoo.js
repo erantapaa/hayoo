280166fcf1be635422b52524e38201af62cf8a67
Erik Rantapaa <erantapaa@gmail.com>
2016-05-31 20:50:24 -0500

    New expand/collapse control for doc sections.

--- hayooFrontend/data/hayoo.js
+++ hayooFrontend/data/hayoo.js
@@ -17,40 +17,64 @@ function makeAutocomplete() {
     });
 }
 
+/* Click handler for the expander/collapser control.
+ * me is the <p> element which was clicked.
+ * We assume that me.parentNode.nextSibling will
+ * be the more div.
+ */
+function toggle_docs(me) {
+
+  var div = me.parentNode.nextSibling // should be the more div
+
+  $(div).children().each(function(i,e) {
+    if (i > 0) { $(e).toggle() }
+  });
+
+  if (me.className == "collapser") {
+    me.className = "expander"
+  } else {
+    me.className = "collapser"
+  }
+  return false;
+}
+
+/*
+ * makeMores adds a more-toggle div just before any more div
+ * which needs to have an control to expand/collapse the
+ * documentation section.
+ *
+ * The children of a more div will be a sequence of P and PRE
+ * elements. We always the first child visible. We expand/collapse
+ * the documentation by toggling the other children to be visible
+ * or hidden.
+ *
+ * The more-toggle div uses position: absolute to hang itself
+ * on the left side of the more div.
+ */
 
 function makeMores (target) {
-    var showChar = 150;
-    var maxChar = 200;
-    var ellipsestext = "...";
-    var moretext = "more";
-    var lesstext = "less";
+
     var t = target ? target : document;
     $(t).find('.more').each(function() {
         var content = $(this).html();
         var textContent = $(this).text();
 
-        if(textContent.length > maxChar) {
+        $(this).html(textContent)
 
-            var c = textContent.substr(0, showChar);
+        var need_toggle = false  // do we need to add a more-toggle div?
+        $(this).children().each(function(i,e) {
+            if (i > 0) { $(e).toggle(); need_toggle = true; }
+        })
 
-
-            var html = '<div class="preview">' + c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<a href="" class="morelink">'+moretext+'</a></span></div>'
-                     + '<div class="content" style="display: none;"">' + content + '<a href="" class="lesslink">'+lesstext+'</a></div>';
-            $(this).html(html);
+        if (need_toggle) {
+            var div = document.createElement("div")
+            div.innerHTML = '<p class="expander" onclick="toggle_docs(this)">&nbsp;'
+            div.className = "more-toggle"
+            this.parentNode.insertBefore(div, this)
         }
 
     });
 
-    $(t).find(".morelink").click(function(){
-        $(this).parent().hide()
-        $(this).parent().next().show()
-        return false;
-    });
-    $(t).find(".lesslink").click(function(){
-        $(this).parent().hide()
-        $(this).parent().prev().show()
-        return false;
-    });
 }
 
 var page = 1
